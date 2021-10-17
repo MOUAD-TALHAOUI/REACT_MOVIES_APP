a5243a79c544734d99659f7b4958aa153aeba369
MOUAD-TALHAOUI <talhaouimouad02@gmail.com>
2021-10-17 17:57:19 +0100

    gh-pages deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect,useState} from 'react';
 import './App.css';
 import Movie from './components/Movie';
 
-const feature_api="http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const feature_api="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const search_api ="http://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 function App() {
 
